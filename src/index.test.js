8700f501b7c35f1d4a6be48e1e6be1693039c8d1
Mir Mufaqam Ali <mannanali413@gmail.com>
2017-12-10 11:45:40 +0530

    chore(releasing): Add travis config, conventional commit and semantic-release
    closes #1

---
+++ src/index.test.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect;
+var starWars = require('./index');
+
+describe('mufaqams-starswars-names', function(){
+  describe('all', function(){
+    it('should be an array of strings', function(){
+      expect(starWars.all).to.satisfy(isArrayOfStrings);
+
+      function isArrayOfStrings(arr){
+        return arr.every(function(item){
+          return typeof(item) === 'string';
+        });
+      }
+    });
+
+    it('should contain Luke Skywalker', function(){
+      expect(starWars.all).to.include('Luke Skywalker');
+    });
+  });
+
+  describe('random', function(){
+    it('should return a random string', function(){
+      var randomEntry = starWars.random();
+      expect(starWars.all).to.include(randomEntry);
+    });
+  });
+});
